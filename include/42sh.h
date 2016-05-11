c9be1e49d8aed6f279b6aeba73691c9136e7bcbe
escorn_t <thomas.escorne@epitech.eu>
2016-05-11 15:58:23 +0100

    +

---
+++ include/42sh.h
@@ -0,0 +1,16 @@
+/*
+** 42sh.h for 42sh in /home/escorn_t/Rendu/PSU/PSU_2015_42sh
+**
+** Made by escorn_t
+** Login   <escorn_t@epitech.net>
+**
+** Started on  Wed May 11 13:26:46 2016 escorn_t
+** Last update Wed May 11 13:29:02 2016 escorn_t
+*/
+
+#ifndef _42SH_H_
+# define _42SH_H_
+
+
+
+#endif /* !_42SH_H_ */

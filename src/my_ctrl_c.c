61de9736a45524a2fa07985a1e45b920a51ba806
villen_l <lucas.villeneuve@epitech.eu>
2016-05-30 10:01:52 +0200

    Fichier auteur + correction my_ctrl

--- src/my_ctrl_c.c
+++ src/my_ctrl_c.c
@@ -5,12 +5,12 @@
 ** Login   <costa_d@epitech.net>
 **
 ** Started on  Thu May 26 18:12:03 2016 Arnaud Costa
-** Last update Thu May 26 18:20:23 2016 Arnaud Costa
+** Last update Thu May 26 17:03:17 2016 Lucas Villeneuve
 */
 
 #include "42sh.h"
 
-void	sig_finder(int sig)
+void	sig_finder(__attribute__((unused))int sig)
 {
   my_putstr("\b\b  \n");
   my_putstr("-->");
